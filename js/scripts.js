99f7d6df81ba7ee070812ce9dd83f054b98a8897
Cynthia Ponce and Victor Guttmann <victor@>
2019-08-13 15:22:34 -0700

    final

---
+++ js/scripts.js
@@ -0,0 +1,265 @@
+$(document).ready(function() {
+  $("#card1front").click(function() {
+    $("#card1back").show();
+    $("#card1front").hide();
+  });
+});
+
+$(document).ready(function() {
+  $("#card1back").click(function() {
+    $("#card1front").show();
+    $("#card1back").hide();
+  });
+});
+
+$(document).ready(function() {
+  $("#card2front").click(function() {
+    $("#card2back").show();
+    $("#card2front").hide();
+  });
+});
+
+$(document).ready(function() {
+  $("#card2back").click(function() {
+    $("#card2front").show();
+    $("#card2back").hide();
+  });
+});
+
+$(document).ready(function() {
+  $("#card3front").click(function() {
+    $("#card3back").show();
+    $("#card3front").hide();
+  });
+});
+
+$(document).ready(function() {
+  $("#card3back").click(function() {
+    $("#card3front").show();
+    $("#card3back").hide();
+  });
+});
+
+$(document).ready(function() {
+  $("#card4front").click(function() {
+    $("#card4back").show();
+    $("#card4front").hide();
+  });
+});
+
+$(document).ready(function() {
+  $("#card4back").click(function() {
+    $("#card4front").show();
+    $("#card4back").hide();
+  });
+});
+
+$(document).ready(function() {
+  $("#card5front").click(function() {
+    $("#card5back").show();
+    $("#card5front").hide();
+  });
+});
+
+$(document).ready(function() {
+  $("#card5back").click(function() {
+    $("#card5front").show();
+    $("#card5back").hide();
+  });
+});
+
+$(document).ready(function() {
+  $("#card6front").click(function() {
+    $("#card6back").show();
+    $("#card6front").hide();
+  });
+});
+
+$(document).ready(function() {
+  $("#card6back").click(function() {
+    $("#card6front").show();
+    $("#card6back").hide();
+  });
+});
+
+$(document).ready(function() {
+  $("#card7front").click(function() {
+    $("#card7back").show();
+    $("#card7front").hide();
+  });
+});
+
+$(document).ready(function() {
+  $("#card7back").click(function() {
+    $("#card7front").show();
+    $("#card7back").hide();
+  });
+});
+
+$(document).ready(function() {
+  $("#card8front").click(function() {
+    $("#card8back").show();
+    $("#card8front").hide();
+  });
+});
+
+$(document).ready(function() {
+  $("#card8back").click(function() {
+    $("#card8front").show();
+    $("#card8back").hide();
+  });
+});
+
+$(document).ready(function() {
+  $("#card9front").click(function() {
+    $("#card9back").show();
+    $("#card9front").hide();
+  });
+});
+
+$(document).ready(function() {
+  $("#card9back").click(function() {
+    $("#card9front").show();
+    $("#card9back").hide();
+  });
+});
+
+$(document).ready(function() {
+  $("#card10front").click(function() {
+    $("#card10back").show();
+    $("#card10front").hide();
+  });
+});
+
+$(document).ready(function() {
+  $("#card10back").click(function() {
+    $("#card10front").show();
+    $("#card10back").hide();
+  });
+});
+
+$(document).ready(function() {
+  $("#card11front").click(function() {
+    $("#card11back").show();
+    $("#card11front").hide();
+  });
+});
+
+$(document).ready(function() {
+  $("#card11back").click(function() {
+    $("#card11front").show();
+    $("#card11back").hide();
+  });
+});
+
+$(document).ready(function() {
+  $("#card12front").click(function() {
+    $("#card12back").show();
+    $("#card12front").hide();
+  });
+});
+
+$(document).ready(function() {
+  $("#card12back").click(function() {
+    $("#card12front").show();
+    $("#card12back").hide();
+  });
+});
+
+$(document).ready(function() {
+  $("#card13front").click(function() {
+    $("#card13back").show();
+    $("#card13front").hide();
+  });
+});
+
+$(document).ready(function() {
+  $("#card13back").click(function() {
+    $("#card13front").show();
+    $("#card13back").hide();
+  });
+});
+
+$(document).ready(function() {
+  $("#card14front").click(function() {
+    $("#card14back").show();
+    $("#card14front").hide();
+  });
+});
+
+$(document).ready(function() {
+  $("#card14back").click(function() {
+    $("#card14front").show();
+    $("#card14back").hide();
+  });
+});
+
+$(document).ready(function() {
+  $("#card15front").click(function() {
+    $("#card15back").show();
+    $("#card15front").hide();
+  });
+});
+
+$(document).ready(function() {
+  $("#card15back").click(function() {
+    $("#card15front").show();
+    $("#card15back").hide();
+  });
+});
+
+$(document).ready(function() {
+  $("#card16front").click(function() {
+    $("#card16back").show();
+    $("#card16front").hide();
+  });
+});
+
+$(document).ready(function() {
+  $("#card16back").click(function() {
+    $("#card16front").show();
+    $("#card16back").hide();
+  });
+});
+
+$(document).ready(function() {
+  $("#card17front").click(function() {
+    $("#card17back").show();
+    $("#card17front").hide();
+  });
+});
+
+$(document).ready(function() {
+  $("#card17back").click(function() {
+    $("#card17front").show();
+    $("#card17back").hide();
+  });
+});
+
+$(document).ready(function() {
+  $("#card18front").click(function() {
+    $("#card18back").show();
+    $("#card18front").hide();
+  });
+});
+
+$(document).ready(function() {
+  $("#card18back").click(function() {
+    $("#card18front").show();
+    $("#card18back").hide();
+  });
+});
+
+$(document).ready(function() {
+  $("#card19front").click(function() {
+    $("#card19back").show();
+    $("#card19front").hide();
+  });
+});
+
+$(document).ready(function() {
+  $("#card19back").click(function() {
+    $("#card19front").show();
+    $("#card19back").hide();
+  });
+});
